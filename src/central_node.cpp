bb02b8ee664835b858e9e79b04b75aac20df361b
LetiziaCurti <leti_t_be@hotmail.it>
2016-05-26 11:28:52 +0200

    Modifica dei msg
    il central node pubblica su assignment_topic un vettore di assignments

--- src/central_node.cpp
+++ src/central_node.cpp
@@ -12,6 +12,8 @@
 #include "geometry_msgs/Twist.h"
 #include "turtlesim/Pose.h"
 #include "task_assign/AgentStatus.h"  
+#include "task_assign/AssignMsg.h"
+#include "task_assign/IniStatus.h"
 
 
 using namespace std;
@@ -30,8 +32,8 @@ struct agent
     string id;
     bool ready;
     bool status;
+    bool assign_status;
     string type;
-    string assign;
     float x;
     float y;
     float theta;
@@ -42,7 +44,8 @@ struct agent task, robot;
 struct agent rToAssign, tToAssign;
 
 
-map<string,string> map_assignment;  // mappa assignment robot-task
+// map<string,string> map_assignment;  // mappa assignment robot-task
+map<string,agent> map_assignment;  // mappa assignment robot-task
 
 map<string,agent> robots_buffer;
 map<ros::Time,string> robots_time;
@@ -63,57 +66,58 @@ inline const char * const BoolToString(bool b)
 
 
 
-
 // Il master pubblica su "assignment_topic" il messaggio contenente l'assignment, lo status del task viene messo 
 // in tutti i campi del messaggio ad eccezione del campo robot_assign in cui viene messo lo status del robot 
 // corrispondente
 void publishAssignment() 
 {
-    task_assign::AgentStatus status_msg; 
+    task_assign::AssignMsg assign_msg; 
     
-    status_msg.header.stamp = ros::Time::now();
-    status_msg.t = tToAssign.t;
-    status_msg.robot_id = tToAssign.id;
-    status_msg.is_ready = tToAssign.ready;
-    status_msg.status = tToAssign.status; 
-    status_msg.type = tToAssign.type;
-    status_msg.x = tToAssign.x;
-    status_msg.y = tToAssign.y;
-    status_msg.theta = tToAssign.theta;
+    int i = 0;
+    for(auto elem : map_assignment)
+    {
+	assign_msg.assign_vect[i].rob_name = elem.first;
+	assign_msg.assign_vect[i].rob_status = elem.second.assign_status;
+	
+	assign_msg.assign_vect[i].task_name = elem.second.id;
+	assign_msg.assign_vect[i].task_status = elem.second.status;
+	assign_msg.assign_vect[i].task_x = elem.second.x;
+	assign_msg.assign_vect[i].task_y = elem.second.y;
+	assign_msg.assign_vect[i].task_theta = elem.second.theta;
+	
+	i++;
+    }
     
-    //pubblico robot_assign
-    status_msg.robot_assign.t = rToAssign.t;
-    status_msg.robot_assign.id = rToAssign.id;
-    status_msg.robot_assign.ready = rToAssign.ready;
-    status_msg.robot_assign.status = rToAssign.status;  
-    status_msg.robot_assign.type = rToAssign.type;
-    status_msg.robot_assign.x = rToAssign.x;
-    status_msg.robot_assign.y = rToAssign.y;
-    status_msg.robot_assign.theta = rToAssign.theta;
+    
 
     // Wait for the publisher to connect to subscribers
     sleep(1.0);
-    assignment_pub.publish(status_msg);
+    assignment_pub.publish(assign_msg);
     
-    ROS_INFO_STREAM("Master is publishing the assignment: "<< tToAssign.id << " - " << rToAssign.id);
+//     ROS_INFO_STREAM("Master is publishing the assignment: "<< tToAssign.id << " - " << rToAssign.id);
 }
+
+
 
 
 
 // Callback con cui il master legge "assignment_topic" per vedere se ci sono robot che hanno finito, e che quindi tornano disponibili
 // In corrispondenza dei robot liberi il master rimette a false il campo status della struttura_agente_robot corrispondente in 
 // robots_buffer e elimina la coppia che ha terminato da map_assignment
-void FreeCallback(const task_assign::AgentStatus::ConstPtr& status_msg)
+void FreeCallback(const task_assign::AssignMsg::ConstPtr& assign_msg)
 {
-    if(status_msg->is_ready && status_msg->status && !status_msg->robot_assign.status)
+    for(auto elem : assign_msg->assign_vect)
     {
-	ROS_INFO_STREAM("Master is listening the ended assignment: "<< status_msg->robot_id << " - " << status_msg->robot_assign.id);
-	
-// 	tasks_buffer[status_msg->robot_id].status=false;
-	
-	robots_buffer[status_msg->robot_assign.id].status=false;
-	
-	map_assignment.erase(status_msg->robot_assign.id);
+	if(elem.task_status && !elem.rob_status)
+	{
+	    ROS_INFO_STREAM("Master is listening the ended assignment: "<< elem.task_name << " - " << elem.rob_name);
+	    
+// 	    tasks_buffer[elem.task_name].status=false;
+	    
+	    robots_buffer[elem.rob_name].status=false;
+	    
+	    map_assignment.erase(elem.rob_name);
+	}
     }
 }
 
@@ -131,17 +135,17 @@ void FreeCallback(const task_assign::AgentStatus::ConstPtr& status_msg)
 // a true (il master modifica la struttura_agente_robot e la struttura_agente_task corrispondenti dentro i buffers rispettivi)
 // Poi viene pubblicato su "assignment_topic" (con publishAssignment()) il messaggio in cui si ha il task con il robot associato 
 // (il cui status è dentro msg.robot_assign)
-void TotalCallback(const task_assign::AgentStatus::ConstPtr& status_msg)
+void TotalCallback(const task_assign::IniStatus::ConstPtr& status_msg)
 {
     //check: deve essere arrivato qualcosa
     if(status_msg->is_ready && status_msg->x!=-1 && status_msg->y!=-1 && status_msg->theta!=200)
     {
         ROS_INFO_STREAM("The master is listening the agent " << status_msg->robot_id << " with status " << BoolToString(status_msg->status));
 	
-	map<string,string>::iterator it;
+	map<string,agent>::iterator it;
 	for (it=map_assignment.begin(); it!=map_assignment.end(); ++it)
 	{
-	    if(status_msg->robot_id==it->first || status_msg->robot_id==it->second) break;
+	    if(status_msg->robot_id==it->first || status_msg->robot_id==it->second.id) break;
 	}
 	
 	if(it==map_assignment.end()) 
@@ -248,15 +252,16 @@ void TotalCallback(const task_assign::AgentStatus::ConstPtr& status_msg)
 		giaInAssign = false;
 		for(auto elem : map_assignment)
 		{
-		    if(rToAssign.id==elem.first || tToAssign.id==elem.second)
+		    if(rToAssign.id==elem.first || tToAssign.id==elem.second.id)
 		      giaInAssign = true;
 		}
 		if(map_assignment.size()<NUM_SPORTELLI && !giaInAssign)
 		{
 		    rToAssign.status=true;
 		    tToAssign.status=true;
+		    tToAssign.assign_status=true;
 		    
-		    map_assignment[rToAssign.id]=tToAssign.id;
+		    map_assignment[rToAssign.id]=tToAssign;
 		
 		    robots_buffer[rToAssign.id].status=true;
 		    tasks_buffer[tToAssign.id].status=true;
@@ -268,7 +273,7 @@ void TotalCallback(const task_assign::AgentStatus::ConstPtr& status_msg)
 	      
 	    for(auto elem : map_assignment)
 	    {
-		ROS_INFO_STREAM("In map_assignment c'e': "<< elem.first <<" - "<<elem.second);
+		ROS_INFO_STREAM("In map_assignment c'e': "<< elem.first <<" - "<<elem.second.id);
 	    }
 	      
 	}
@@ -289,7 +294,8 @@ int main(int argc, char **argv)
     task_status_sub = node.subscribe("task_arrival_topic", 20, &TotalCallback);   
     robot_status_sub = node.subscribe("robot_arrival_topic", 20, &TotalCallback);
     
-    assignment_pub = node.advertise<task_assign::AgentStatus>("assignment_topic", 10);
+//     assignment_pub = node.advertise<task_assign::AssignMsg>("assignment_topic", 10);
+    assignment_pub = node.advertise<task_assign::AssignMsg>("assignment_topic", 10);
     assignment_sub = node.subscribe("assignment_topic", 20, &FreeCallback);
     sleep(1);
     

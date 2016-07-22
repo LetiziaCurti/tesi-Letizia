920bd9ee3fbd9aab04054a506cc836ad1b7220f6
LetiziaCurti <leti_t_be@hotmail.it>
2016-07-22 11:17:43 +0200

    Update task_manager.cpp

--- src/task_manager.cpp
+++ src/task_manager.cpp
@@ -1,4 +1,6 @@
 // Nodo che modella il task manager
+//  tiene traccia dei nuovi task (pubblicati dal nodo_utenti) e di quelli che vengono completati (pubblicati dal 
+//  motion_planner), pubblica al central_node il vettore degli n(t) task da assegnare
 
 
 #include <iostream>
@@ -7,6 +9,7 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
 #include "task_assign/vect_task.h"
+#include "task_assign/AssignMsg.h"
 
 
 inline const char * const BoolToString(bool b)
@@ -42,31 +45,118 @@ struct couple_task
     float theta2;
 };
 
+// gli elementi dei vettori potrebbero essere già dei type task_Assign::task così quando il nodo deve pubblicare il vettore 
+// task_to_assign è già un vettore di elementi task, altrimenti le struct couple_task vanno copiate tutte nei type masg
+vector<struct couple_task> new_task;          		//vettore in cui vengono messi i nuovi task in arrivo da users_node
+vector<struct couple_task> executed_task;		//vettore in cui vengono messi i task eseguiti comunicati dal motion planner
 
-vector<struct couple_task> new_task;          	//vettore in cui vengono messi i nuovi task in arrivo da users_node
-vector<struct couple_task> exec_task;		//vettore in cui vengono messi i task eseguiti comunicati dal motion planner
-vector<struct couple_task> task_to_assign;    	//n(t): vettore dei task da assegnare che cambia nel tempo
+vector<task_assign::task> task_to_assign;    		//n(t): vettore dei task da assegnare che cambia nel tempo
 
+int dim_n(0);
 
 
 // Legge "task_arrival_topic" e mette i nuovi task nel vettore new_task
 void NewCallback(const task_assign::vect_task::ConstPtr& msg)
 {
-
+    bool add_task(true);
+    struct couple_task task;
+    
+    for(auto elem : msg->task_vect)
+    {
+	// vedo se elem sta già in new_task
+	for(auto newel : new_task)
+	{
+	    if(newel.name1 == elem.name1 && newel.name2 == elem.name2)
+	      add_task = false;
+	}
+	
+	if(add_task)
+	{
+	   task.ar_time = elem.ar_time;
+	   task.id1 = elem.id1;
+	   task.name1 = elem.name1;
+	   task.status1 = elem.status1;
+	   task.wait1 = elem.wait1;
+	   task.x1 = elem.x1;
+	   task.y1 = elem.y1;
+	   task.theta1 = elem.theta1;
+	   
+	   task.id2 = elem.id2;
+	   task.name2 = elem.name2;
+	   task.status2 = elem.status2;
+	   task.wait2 = elem.wait2;
+	   task.x2 = elem.x2;
+	   task.y2 = elem.y2;
+	   task.theta2 = elem.theta2;
+	   
+	   new_task.push_back(task);
+	}
+    }
 }
 
 
 // Legge "task_exec_topic" e mette i nuovi task nel vettore exec_task
 void ExecCallback(const task_assign::vect_task::ConstPtr& msg)
-{
-
+{   
+    bool add_task(true);
+    struct couple_task task;
+    
+    for(auto elem : msg->task_vect)
+    {
+	// vedo se elem sta già in executed_task
+	for(auto newel : executed_task)
+	{
+	    if(newel.name1 == elem.name1 && newel.name2 == elem.name2)
+	      add_task = false;
+	}
+	
+	if(add_task)
+	{
+	   task.ar_time = elem.ar_time;
+	   task.id1 = elem.id1;
+	   task.name1 = elem.name1;
+	   task.status1 = elem.status1;
+	   task.wait1 = elem.wait1;
+	   task.x1 = elem.x1;
+	   task.y1 = elem.y1;
+	   task.theta1 = elem.theta1;
+	   
+	   task.id2 = elem.id2;
+	   task.name2 = elem.name2;
+	   task.status2 = elem.status2;
+	   task.wait2 = elem.wait2;
+	   task.x2 = elem.x2;
+	   task.y2 = elem.y2;
+	   task.theta2 = elem.theta2;
+	   
+	   executed_task.push_back(task);
+	}
+    }
 }
 
 
-// Pubblica al central_node il vettore dei task da eseguire task_to_assign
+// mette insieme executed_task(k), new_task(k) e task_to_assign(k-1) ed elabora un nuovo task_to_assign(k)
+void taskManagement()
+{
+    dim_n = task_to_assign.size();
+}
+
+
+// Pubblica al master il vettore dei task da eseguire task_to_assign
 void publishTaskToAssign()
 {
-    task_assign::vect_task vect_msg;
+    task_assign::vect_task assignment_msg; 
+    
+    assignment_msg.task_vect = task_to_assign;
+
+    // Wait for the publisher to connect to subscribers
+    sleep(1.0);
+    task_ass_pub.publish(assignment_msg);
+    
+//     for(auto elem : task_to_assign)
+//     {
+// 	ROS_INFO_STREAM("Task Manager is publishing the assignment: "<< elem.task_id << " - " << elem.rob_id);
+//     }
 }
 
 
@@ -90,6 +180,11 @@ int main(int argc, char **argv)
     ros::Rate rate(10);
     while (ros::ok()) 
     {
+	taskManagement();
+	
+	if(dim_n > 0)
+	    publishTaskToAssign();
+	
 	ros::spinOnce();
 	rate.sleep();
     }

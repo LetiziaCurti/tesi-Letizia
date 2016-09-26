c4c333f093ce943fd928af3472326f30fa7d156f
LetiziaCurti <leti_t_be@hotmail.it>
2016-09-27 00:22:50 +0200

    Update users_node.cpp

--- src/users_node.cpp
+++ src/users_node.cpp
@@ -21,154 +21,158 @@ void CreateNewTask()
 {
     task_assign::task newTask;
     
-    // aggiungo il task1
-    newTask.ar_time = 5;
-    newTask.name = "task1";
-    newTask.name1 = "taska";
-    newTask.id1 = 1;
-    newTask.status1 = true;
-    newTask.wait1 = 3;
-    newTask.x1 = 7;
-    newTask.y1 = 8.5;
-    newTask.theta1 = -1.5;
-    newTask.name2 = "taskb";
-    newTask.id2 = 3;
-    newTask.status2 = true;
-    newTask.wait2 = 5;
-    newTask.x2 = 16;
-    newTask.y2 = 15;
-    newTask.theta2 = -1.5; 
-    new_task_vect.push_back(newTask);
+    // Leggi tutte le info da un file yaml e mettile al posto di new_task_vect
+    YAML::Node node_conf = YAML::LoadFile("/home/letizia/catkin_ws/src/task_assign/config/tasks_config.yaml");
+    const YAML::Node& node_test1 = node_conf["TASK"];
+
+    for (std::size_t i = 0; i < node_test1.size(); i++) 
+    {
+	const YAML::Node& node_test2 = node_test1[i];
+	newTask.name = node_test2["name"].as<std::string>();
+	std::cout << "Name: " << node_test2["name"].as<std::string>() << std::endl;
+	newTask.ar_time = node_test2["arrt"].as<double>();
+	std::cout << "Arrival time: " << node_test2["arrt"].as<double>() << std::endl;
+	
+	const YAML::Node& node_test3 = node_test2["tasks"];
+	for (std::size_t i = 0; i < node_test3.size(); i++) 
+	{
+	    const YAML::Node& node_test4 = node_test3[i];
+	    if(i==0)
+	    {
+		newTask.id1 = node_test4["id"].as<double>();
+		newTask.wait1 = node_test4["wait"].as<double>();
+	    }
+	    else if(i==1)
+	    {
+		newTask.id2 = node_test4["id"].as<double>();
+		newTask.wait2 = node_test4["wait"].as<double>();
+	    }
+	    
+	    std::cout << "Id"<< i+1 << ": " << node_test4["id"].as<double>() << std::endl;
+	    std::cout << "Wait: " << node_test4["wait"].as<double>() << std::endl;
+	    
+	    const YAML::Node& node_pos = node_test4["position"];
+	    for (std::size_t j = 0; j < node_pos.size(); j++) 
+	    {
+		if(i==0)
+		{
+		    if(j==0)
+		    {
+			newTask.x1 = node_pos[j].as<double>();
+			std::cout << "x: " << node_pos[j].as<double>() << std::endl;
+		    }
+		    else  if(j==1)
+		    {
+			newTask.y1 = node_pos[j].as<double>();
+			std::cout << "y: " << node_pos[j].as<double>() << std::endl;
+		    }
+		    else  if(j==2)
+		    {
+			newTask.theta1 = node_pos[j].as<double>();
+			std::cout << "theta: " << node_pos[j].as<double>() << std::endl;
+		    }
+		}
+		else if(i==1)
+		{
+		    if(j==0)
+		    {
+			newTask.x2 = node_pos[j].as<double>();
+			std::cout << "x: " << node_pos[j].as<double>() << std::endl;
+		    }
+		    else  if(j==1)
+		    {
+			newTask.y2 = node_pos[j].as<double>();
+			std::cout << "y: " << node_pos[j].as<double>() << std::endl;
+		    }
+		    else  if(j==2)
+		    {
+			newTask.theta2 = node_pos[j].as<double>();
+			std::cout << "theta: " << node_pos[j].as<double>() << std::endl;
+		    }
+		}
+	    }
+	}
+	
+	new_task_vect.push_back(newTask);
+    }
     
-    // aggiungo il task2
-    newTask.ar_time = 10;
-    newTask.name = "task2";
-    newTask.name1 = "taskc";
-    newTask.id1 = 4;
-    newTask.status1 = true;
-    newTask.wait1 = 3;
-    newTask.x1 = 5.5;
-    newTask.y1 = 5.5;
-    newTask.theta1 = -1.5;
-    newTask.name2 = "taskd";
-    newTask.id2 = 2;
-    newTask.status2 = true;
-    newTask.wait2 = 10;
-    newTask.x2 = 2;
-    newTask.y2 = 19;
-    newTask.theta2 = -1.5; 
-    new_task_vect.push_back(newTask);
-    
-    // aggiungo il task3
-    newTask.ar_time = 15;
-    newTask.name = "task3";
-    newTask.name1 = "taske";
-    newTask.id1 = 5;
-    newTask.status1 = true;
-    newTask.wait1 = 3;
-    newTask.x1 = 18;
-    newTask.y1 = 10;
-    newTask.theta1 = -1.5;
-    newTask.name2 = "taskf";
-    newTask.id2 = 6;
-    newTask.status2 = true;
-    newTask.wait2 = 15;
-    newTask.x2 = 1;
-    newTask.y2 = 1;
-    newTask.theta2 = -1.5; 
-    new_task_vect.push_back(newTask);
-    
-        // aggiungo il task4
-    newTask.ar_time = 10;
-    newTask.name = "task4";
-    newTask.name1 = "taskg";
-    newTask.id1 = 7;
-    newTask.status1 = true;
-    newTask.wait1 = 20;
-    newTask.x1 = 18;
-    newTask.y1 = 3;
-    newTask.theta1 = -1.5;
-    newTask.name2 = "taskh";
-    newTask.id2 = 8;
-    newTask.status2 = true;
-    newTask.wait2 = 5;
-    newTask.x2 = 11;
-    newTask.y2 = 17;
-    newTask.theta2 = -1.5; 
-    new_task_vect.push_back(newTask);
+//     // aggiungo il task1
+//     newTask.ar_time = 5;
+//     newTask.name = "task1";
+//     newTask.name1 = "taska";
+//     newTask.id1 = 1;
+//     newTask.status1 = true;
+//     newTask.wait1 = 3;
+//     newTask.x1 = 7;
+//     newTask.y1 = 8.5;
+//     newTask.theta1 = -1.5;
+//     newTask.name2 = "taskb";
+//     newTask.id2 = 3;
+//     newTask.status2 = true;
+//     newTask.wait2 = 5;
+//     newTask.x2 = 16;
+//     newTask.y2 = 15;
+//     newTask.theta2 = -1.5; 
+//     new_task_vect.push_back(newTask);
+//     
+//     // aggiungo il task2
+//     newTask.ar_time = 10;
+//     newTask.name = "task2";
+//     newTask.name1 = "taskc";
+//     newTask.id1 = 4;
+//     newTask.status1 = true;
+//     newTask.wait1 = 3;
+//     newTask.x1 = 5.5;
+//     newTask.y1 = 5.5;
+//     newTask.theta1 = -1.5;
+//     newTask.name2 = "taskd";
+//     newTask.id2 = 2;
+//     newTask.status2 = true;
+//     newTask.wait2 = 10;
+//     newTask.x2 = 2;
+//     newTask.y2 = 19;
+//     newTask.theta2 = -1.5; 
+//     new_task_vect.push_back(newTask);
+//     
+//     // aggiungo il task3
+//     newTask.ar_time = 15;
+//     newTask.name = "task3";
+//     newTask.name1 = "taske";
+//     newTask.id1 = 5;
+//     newTask.status1 = true;
+//     newTask.wait1 = 3;
+//     newTask.x1 = 18;
+//     newTask.y1 = 10;
+//     newTask.theta1 = -1.5;
+//     newTask.name2 = "taskf";
+//     newTask.id2 = 6;
+//     newTask.status2 = true;
+//     newTask.wait2 = 15;
+//     newTask.x2 = 1;
+//     newTask.y2 = 1;
+//     newTask.theta2 = -1.5; 
+//     new_task_vect.push_back(newTask);
+//     
+//         // aggiungo il task4
+//     newTask.ar_time = 10;
+//     newTask.name = "task4";
+//     newTask.name1 = "taskg";
+//     newTask.id1 = 7;
+//     newTask.status1 = true;
+//     newTask.wait1 = 20;
+//     newTask.x1 = 18;
+//     newTask.y1 = 3;
+//     newTask.theta1 = -1.5;
+//     newTask.name2 = "taskh";
+//     newTask.id2 = 8;
+//     newTask.status2 = true;
+//     newTask.wait2 = 5;
+//     newTask.x2 = 11;
+//     newTask.y2 = 17;
+//     newTask.theta2 = -1.5; 
+//     new_task_vect.push_back(newTask);
 }
 
-// struct couple_task
-// {
-//     double ar_time;
-//     string name1;
-//     int id1;
-//     bool status1;
-//     double wait1;
-//     float x1;
-//     float y1;
-//     float theta1;
-//     
-//     string name2;
-//     int id2;
-//     bool status2;
-//     double wait2;
-//     float x2;
-//     float y2;
-//     float theta2;
-// };
-
-// namespace YAML {
-// template<>
-// struct convert<task_assign::task> {
-//   
-//     static Node encode(const task_assign::task& rhs) {
-// 	Node node;
-// 	node.push_back(rhs.name);
-// 	node.push_back(rhs.ar_time);
-// 	node.push_back(rhs.name1);
-// 	node.push_back(rhs.id1);
-// 	node.push_back(rhs.x1);
-// 	node.push_back(rhs.y1);
-// 	node.push_back(rhs.theta1);
-// 	node.push_back(rhs.wait1);
-// 	node.push_back(rhs.status1);
-// 	node.push_back(rhs.name2);
-// 	node.push_back(rhs.id2);
-// 	node.push_back(rhs.x2);
-// 	node.push_back(rhs.y2);
-// 	node.push_back(rhs.theta2);
-// 	node.push_back(rhs.wait2);
-// 	node.push_back(rhs.status2);
-// 	return node;
-//     }
-// 
-//     static bool decode(const Node& node, task_assign::task& rhs) {
-// //     if(!node.IsSequence() || node.size() != 13) {
-// //       return false;
-// //     }
-// 
-// 	rhs.name = node["name"].as<string>();
-// 	rhs.ar_time = node["ritardo"].as<double>();
-// 	rhs.name1 = node["name1"].as<string>();
-// 	rhs.id1 = node["id1"].as<int>();
-// 	rhs.x1 = node["position1"][0].as<double>();
-// 	rhs.y1 = node["position1"][1].as<double>();
-// 	rhs.theta1 = node["position1"][2].as<double>();
-// 	rhs.wait1 = node["wait1"].as<double>();
-// 	rhs.status1 = node["status1"].as<bool>();
-// 	rhs.name2 = node["name2"].as<string>();
-// 	rhs.id2 = node["id2"].as<int>();
-// 	rhs.x2 = node["position2"][0].as<double>();
-// 	rhs.y2 = node["position2"][1].as<double>();
-// 	rhs.theta2 = node["position2"][2].as<double>();
-// 	rhs.wait2 = node["wait2"].as<double>();
-// 	rhs.status2 = node["status2"].as<bool>();
-// 	
-// 	return true;
-//     }
-// };
-// }
 
 
 
@@ -183,7 +187,7 @@ void publishVectTask()
     
     for(auto elem : vect_msg.task_vect)
     {
-	ROS_INFO_STREAM("The users node is publishing the task: "<< elem.name << " whit the couple " << elem.name1 << " - " << elem.name2);
+	ROS_INFO_STREAM("The users node is publishing the task: "<< elem.name << " with the couple " << elem.id1 << " - " << elem.id2);
     }
 }
 
@@ -207,49 +211,6 @@ int main(int argc, char **argv)
     sleep(1);
     
     CreateNewTask();
-    
-    // Leggi tutte le info da un file yaml e mettile al posto di new_task_vect
-    YAML::Node node_conf = YAML::LoadFile("/home/letiziacurti/catkin_ws/src/task_assign/config/tasks_config.yaml");
-    const YAML::Node& node_test1 = node_conf["TASK"];
-
-    for (std::size_t i = 0; i < node_test1.size(); i++) 
-    {
-	const YAML::Node& node_test2 = node_test1[i];
-	std::cout << "Name: " << node_test2["name"].as<std::string>() << std::endl;
-	std::cout << "Arrival time: " << node_test2["arrt"].as<double>() << std::endl;
-	
-	const YAML::Node& node_test3 = node_test2["tasks"];
-	for (std::size_t i = 0; i < node_test3.size(); i++) 
-	{
-	    const YAML::Node& node_test4 = node_test3[i];
-	    cout << "parte " << i << " del task" << endl;
-	    std::cout << "Id: " << node_test4["id"].as<double>() << std::endl;
-	    std::cout << "Wait: " << node_test4["wait"].as<double>() << std::endl;
-	    
-	    const YAML::Node& node_pos = node_test4["position"];
-	    for (std::size_t i = 0; i < node_pos.size(); i++) 
-	    {
-		if(i==0)
-		    std::cout << "x: " << node_pos[i].as<double>() << std::endl;
-		else  if(i==1)
-		    std::cout << "y: " << node_pos[i].as<double>() << std::endl;
-		else  if(i==2)
-		      std::cout << "theta: " << node_pos[i].as<double>() << std::endl;
-	    }
-	    
-// 	    const YAML::Node& node_test5 = node_test4["position"];
-// 	    std::cout << "Position: " << endl;
-// 	    for (std::size_t i = 0; i < node_test5.size(); i++) 
-// 	    {
-// 		cout << "x: " << node_test5["x"].as<double>() << endl;
-// 		std::cout << "y: " << node_test5["y"].as<double>() << std::endl;
-// 		std::cout << "theta: " << node_test5["theta"].as<double>() << std::endl;
-// 	    }
-// 	    cout << " x: " << node_test5["x"].as<double>() << " y: " << node_test5["y"].as<double>() << " theta: " << node_test5["theta"].as<double>() << endl;	  
-
-	}
-    }
-    
     
     ros::Rate rate(10);
     while (ros::ok()) 

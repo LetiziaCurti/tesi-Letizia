65c66e7395c17421253a192570bb3da8cd62d428
LetiziaCurti <leti_t_be@hotmail.it>
2016-11-12 10:20:52 +0100

    aggiornate mesh

--- src/robot.cpp
+++ src/robot.cpp
@@ -597,9 +597,9 @@ public:
 	marker.pose.orientation.w = Quat.w;
 
 	// Set the scale of the marker -- 1x1x1 here means 1m on a side
-	marker.scale.x = 0.1;
-	marker.scale.y = 0.1;
-	marker.scale.z = 0.05;
+	marker.scale.x = 0.06;
+	marker.scale.y = 0.08;
+	marker.scale.z = 0.03;
 
 	// Set the color -- be sure to set alpha to something non-zero!
 	marker.color.r = r;
